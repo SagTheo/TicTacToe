5875bebdca7f0090b71e4c833e98791279a487ad
Theo <sagniez.theo@laposte.net>
2022-07-07 10:09:59 +0200

    Started working on logic to check if there are 3 symbols in a row

--- scripts.js
+++ scripts.js
@@ -1,9 +1,37 @@
 const divs = document.querySelectorAll('.container div')
+const box1 = document.querySelector('#1')
+const box2 = document.querySelector('#2')
+const box3 = document.querySelector('#3')
+const box4 = document.querySelector('#4')
+const box5 = document.querySelector('#5')
+const box6 = document.querySelector('#6')
+const box7 = document.querySelector('#7')
+const box8 = document.querySelector('#8')
+const box9 = document.querySelector('#9')
+
+const threes = [
+    [box1, box2, box3],
+    [box4, box5, box6],
+    [box7, box8, box9],
+    [box1, box4, box7],
+    [box2, box5, box8],
+    [box3, box6, box9],
+    [box1, box5, box9],
+    [box3, box5, box7]
+]
 
 let player1 = true
 
 const cross = 'x-lg.svg'
 const circle = 'circle.svg'
+
+const checkFullRow = () => {
+    threes.forEach(array => {
+        array.forEach(box => {
+            
+        })
+    })
+}
 
 divs.forEach(div => {
     div.addEventListener('click', (e) => {
