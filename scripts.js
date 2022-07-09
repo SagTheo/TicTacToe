adb8e802dd7b1409bf6c64349abc593b0a7f0f23
Theo <sagniez.theo@laposte.net>
2022-07-09 20:35:24 +0200

    Kept working on checkFullRow function

--- scripts.js
+++ scripts.js
@@ -1,36 +1,32 @@
 const divs = document.querySelectorAll('.container div')
-const box1 = document.querySelector('#1')
-const box2 = document.querySelector('#2')
-const box3 = document.querySelector('#3')
-const box4 = document.querySelector('#4')
-const box5 = document.querySelector('#5')
-const box6 = document.querySelector('#6')
-const box7 = document.querySelector('#7')
-const box8 = document.querySelector('#8')
-const box9 = document.querySelector('#9')
+const box1 = document.querySelector('#r1-c1-d1')
+const box2 = document.querySelector('#r1-c2')
+const box3 = document.querySelector('#r1-c3-d2')
+const box4 = document.querySelector('#r2-c1')
+const box5 = document.querySelector('#r2-c2-d1-d2')
+const box6 = document.querySelector('#r2-c3')
+const box7 = document.querySelector('#r3-c1-d2')
+const box8 = document.querySelector('#r3-c2')
+const box9 = document.querySelector('#r3-c3-d1')
 
-const threes = [
-    [box1, box2, box3],
-    [box4, box5, box6],
-    [box7, box8, box9],
-    [box1, box4, box7],
-    [box2, box5, box8],
-    [box3, box6, box9],
-    [box1, box5, box9],
-    [box3, box5, box7]
-]
+const threes = {
+    'r1': [box1, box2, box3],
+    'r2': [box4, box5, box6],
+    'r3': [box7, box8, box9],
+    'c1': [box1, box4, box7],
+    'c2': [box2, box5, box8],
+    'c3': [box3, box6, box9],
+    'd1': [box1, box5, box9],
+    'd2': [box3, box5, box7]
+}
 
 let player1 = true
 
 const cross = 'x-lg.svg'
 const circle = 'circle.svg'
 
-const checkFullRow = () => {
-    threes.forEach(array => {
-        array.forEach(box => {
-            
-        })
-    })
+const checkFullRow = (row) => {
+    console.log(row)
 }
 
 divs.forEach(div => {
@@ -52,6 +48,8 @@ divs.forEach(div => {
                     player1 = !player1
                 }
             }
+
+            checkFullRow(e.target.getAttribute('id'))
         }
     })
 })
